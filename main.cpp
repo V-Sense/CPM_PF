477d490d88dc14b0ff21347d3ed0b569b9df3217
Martin Alain <alainm@scss.tcd.ie>
2019-06-06 18:32:38 +0100

    Add utils.* files and move all image data conversion from main to utils.

--- main.cpp
+++ main.cpp
@@ -3,122 +3,11 @@
 #include "CPM_Tip2017Mod/OpticFlowIO.h"
 #include "PFilter/PermeabilityFilter.h"
 #include "flowIO.h"
+#include "utils.h"
 extern "C" {
 #include "PFilter/variational/variational.h"
-#include "PFilter/variational/io.h"
 }
 
-
-// draw each match as a 3x3 color block
-void Match2Flow(FImage& inMat, FImage& ou, FImage& ov, int w, int h)
-{
-	if (!ou.matchDimension(w, h, 1)){
-		ou.allocate(w, h, 1);
-	}
-	if (!ov.matchDimension(w, h, 1)){
-		ov.allocate(w, h, 1);
-	}
-	ou.setValue(UNKNOWN_FLOW);
-	ov.setValue(UNKNOWN_FLOW);
-	int cnt = inMat.height();
-	for (int i = 0; i < cnt; i++){
-		float* p = inMat.rowPtr(i);
-		float x = p[0];
-		float y = p[1];
-		float u = p[2] - p[0];
-		float v = p[3] - p[1];
-
-		for (int di = -1; di <= 1; di++){
-            for (int dj = -1; dj <= 1; dj++){
-				int tx = ImageProcessing::EnforceRange(x + dj, w);
-				int ty = ImageProcessing::EnforceRange(y + di, h);
-				ou[ty*w + tx] = u;
-				ov[ty*w + tx] = v;
-			}
-		}
-
-	}
-}
-
-void Match2Mat2f(FImage matches, Mat2f &flow)
-{
-	int h = flow.rows;
-    int w = flow.cols;
-	int cnt = matches.height();
-	for (int i = 0; i < cnt; i++){
-		float* p = matches.rowPtr(i);
-		float x = p[0];
-		float y = p[1];
-		float u = p[2] - p[0];
-		float v = p[3] - p[1];
-
-		for (int di = -1; di <= 1; di++){
-            for (int dj = -1; dj <= 1; dj++){
-				int tx = ImageProcessing::EnforceRange(x + dj, w);
-				int ty = ImageProcessing::EnforceRange(y + di, h);
-				flow(ty, tx)[0] = u;
-				flow(ty, tx)[1] = v;
-			}
-		}
-
-	}
-}
-
-void WriteMatches(const char *filename, FImage& inMat)
-{
-	int len = inMat.height();
-	FILE *fid = fopen(filename, "w");
-	for (int i = 0; i < len; i++){
-		float x1 = inMat[4 * i + 0];
-		float y1 = inMat[4 * i + 1];
-		float x2 = inMat[4 * i + 2];
-		float y2 = inMat[4 * i + 3];
-		fprintf(fid, "%.0f %.0f %.0f %.0f\n", x1, y1, x2, y2);
-	}
-	fclose(fid);
-}
-
-void FImage2image_t(FImage mu, image_t* wx) {
-    if(!(mu.nelements() > 0))
-        cout<<"Can't convert from empty image!"<<endl;
-    memcpy(wx->data, mu.pData, sizeof(float) * mu.nelements());
-}
-
-void Mat2f2image_t_uv(Mat2f flow, image_t* wx, image_t* wy) {
-for (size_t y = 0; y < flow.rows; ++y) {
-        for (size_t x = 0; x < flow.cols; ++x) {
-            wx->data[y * wx->stride + x] = flow(y, x)[0];
-            wy->data[y * wy->stride + x] = flow(y, x)[1];
-        }
-    }
-}
-
-void Mat3f2FImage(Mat3f in, FImage &out) {
-    for (size_t y = 0; y < in.rows; ++y) {
-        for (size_t x = 0; x < in.cols; ++x) {
-                float pixBGR[3] = {in(y, x)[0], in(y, x)[1], in(y, x)[2]}; // Opencv stores pixels in BGR order
-                out.setPixel(y, x, pixBGR);
-        }
-    }
-}
-
-void Mat3f2color_image_t(Mat3f in, color_image_t *out) {
-    for (size_t y = 0; y < in.rows; ++y) {
-        for (size_t x = 0; x < in.cols; ++x) {
-                out->c1[y * out->stride + x] = in(y, x)[0];
-                out->c2[y * out->stride + x] = in(y, x)[1];
-                out->c3[y * out->stride + x] = in(y, x)[2];
-        }
-    }
-}
-
-void image_t2Mat(image_t* in, Mat &out) {
-    for (size_t y = 0; y < out.rows; ++y) {
-        for (size_t x = 0; x < out.cols; ++x) {
-            out.at<float>(y, x) = in->data[y * in->stride + x];
-        }
-    }
-}
 
 void Usage()
 {
@@ -467,15 +356,15 @@ int main(int argc, char** argv)
             }
         }
 
-        flo = pf_temporal_flow_vec[i];
+        Mat2f pf_flow = pf_temporal_flow_vec[i];
 
         variational_params_t flow_params;
         variational_params_default(&flow_params);
-        image_t *wx = image_new(im1->width, im1->height), *wy = image_new(im1->width, im1->height);
+        image_t *flow_x = image_new(im1->width, im1->height), *flow_y = image_new(im1->width, im1->height);
 
-        Mat2f2image_t_uv(flo, wx, wy);
+        Mat2f2image_t_uv(pf_flow, flow_x, flow_y);
 
-        variational(wx, wy, im1, im2, &flow_params);
+        variational(flow_x, flow_y, im1, im2, &flow_params);
 
 
         ostringstream refined_cpmpf_flows_name_flo_builder, refined_cpmpf_flows_name_png_builder;
@@ -485,15 +374,20 @@ int main(int argc, char** argv)
         string refined_cpm_matches_name_flo = refined_cpmpf_flows_name_flo_builder.str();
         string refined_cpm_matches_name_png = refined_cpmpf_flows_name_png_builder.str();
 
+        vector<Mat1f> flow_vec;
+        flow_vec.push_back(Mat1f::zeros(flow_x->width, flow_x->height));
+        flow_vec.push_back(Mat1f::zeros(flow_y->width, flow_y->height));
+        image_t2Mat(flow_x, flow_vec[0]);
+        image_t2Mat(flow_y, flow_vec[1]);
 
-        // CTimer write_flo_time;
-        writeFlowFile(refined_cpm_matches_name_flo.c_str(), wx, wy);
-        // write_flo_time.toc("Write final flo file: ");
+        Mat2f flow_mat;
+        merge(flow_vec, flow_mat);
+        WriteFlowFile(flow_mat, refined_cpm_matches_name_flo.c_str());
 
         // Convert final result to pfm
         // CTimer convert2disp_time;
-        Mat disp(cv::Size(wx->width, wx->height), CV_32F);
-        image_t2Mat(wx, disp);
+        Mat disp(flow_x->width, flow_x->height, CV_32F);
+        image_t2Mat(flow_x, disp);
         disp = -disp;
         // convert2disp_time.toc("Conversion of image_t to Mat: ");
 
